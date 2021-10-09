eced476488a604b278cd294999e797a89fbe0079
Dennis <dlex351279@gmail.com>
2021-10-09 20:09:36 +0800

    rspec with devise

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,15 @@
 FactoryBot.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
-    
+    email
+    password { "password" }
+    password_confirmation { "password" }
   end
 
   factory :mission do
     title {"Mission Name"}
     content {"Content."}
+    user
   end
 end
