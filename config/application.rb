61d7188365d93bc07e4860bc7165a5d13a45e3de
Dennis <dlex351279@gmail.com>
2021-10-10 22:02:18 +0800

    add Taipei time zone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module MissionApp
     config.load_defaults 6.1
     config.generators.assets = false
     config.generators.helper = false
+    config.time_zone = "Taipei"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
